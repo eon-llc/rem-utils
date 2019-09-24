d240748a26903c1facf80e39091e73217888b8d1
Serg Chernata <sergchernata@gmail.com>
2019-09-24 16:17:12 -0400

    initial commit

---
+++ bp-monitor/bp-monitor.sh
@@ -0,0 +1,138 @@
+#!/bin/bash
+#---------------------------------
+# SETUP
+#---------------------------------
+# load variables from config
+source "./config.conf"
+
+# set bash time zone
+TZ=$TIME_ZONE
+
+# full path to config file for convenience
+CONFIG_FILE_PATH="${SCRIPT_DIR}/config.conf"
+
+alerts=()
+now=$(date -u +"%Y-%m-%dT%H:%M:%SZ")
+now_s=$(date -d $now +%s)
+now_n=$(date -d $now +%s%N)
+
+#---------------------------------
+# SCHEDULE THIS SCRIPT AS CRON
+#---------------------------------
+if ! crontab -l | grep -q $SCRIPT_FILE
+then
+  (crontab -l ; echo "* * * * * ${SCRIPT_DIR}/${SCRIPT_FILE}") | crontab -
+fi
+
+#---------------------------------
+# LOG FILE STATE TEST & MAINTENANCE
+#---------------------------------
+log_last_modified_s=$(date -r $LOG_FILE +%s)
+modified_diff=$(( $now_s - $log_last_modified_s ))
+log_byte_size=$(stat -c%s $LOG_FILE)
+
+# if log has not been modified
+# within the last 5 minutes
+if [ $modified_diff -ge 300 ]; then
+    alerts+=( "Node log was last modified $(( modified_diff / 60 )) minutes ago" )
+fi
+
+# if log is larger than specified threshold
+if [ $(( $log_byte_size / 1000000)) -gt $MAX_LOG_SIZE ]; then
+    sudo truncate -s 0 $LOG_FILE
+fi
+
+#---------------------------------
+# TEST CHAIN STATE
+#---------------------------------
+# test "remcli get info" response
+get_info_response="$(remcli get info)"
+
+# if response is empty or that of failed connection
+if [[ -z "${get_info_response// }" ]] || [[ "Failed" =~ ^$get_info_response ]]; then
+    alerts+=( "Failed to receive a response from (remcli get info)" )
+else
+    head_block_num="$(jq '.head_block_num | tonumber' <<< ${get_info_response})"
+    li_block_num="$(jq '.last_irreversible_block_num | tonumber' <<< ${get_info_response})"
+    block_diff=$(( head_block_num - li_block_num ))
+
+    # if the gap between head block and last irreversible
+    # is more than 3 minutes, send an alert
+    if (( block_diff / 2 / 60 > 3 )); then
+        alerts+=( "Current block is ${block_diff} ahead of last irreversible" )
+    fi
+
+    # if last irreversible block has not advanced
+    if [ $LAST_IRREVERSIBLE_BLOCK_NUM -eq $li_block_num ]; then
+        alerts+=( "Last irreversible block is stuck on ${li_block_num}" )
+    fi
+
+    # update last irreversible block number
+    sed -i "s/last_irreversible_block_num=.*/last_irreversible_block_num=$li_block_num/" $CONFIG_FILE_PATH
+fi
+
+#---------------------------------
+# TEST NET PEER STATE
+#---------------------------------
+# test "remcli net peers" last handshake time
+net_peers_response="$(remcli net peers)"
+
+# if response is empty or that of failed connection
+if [[ -z "${net_peers_response// }" ]] || [[ "Failed" =~ ^$net_peers_response ]]; then
+    alerts+=( "Failed to receive a response from (remcli net peers)" )
+else
+    last_handshake=$(jq '.[0].last_handshake.time | tonumber' <<< ${net_peers_response})
+    diff_n=$(( $now_n - $last_handshake ))
+
+    # if peer time is older than 3 minutes, in nanoseconds
+    if [ $diff_n -gt 180000000000 ] ; then
+        alerts+=( "Last peer handshake was $(( diff_n / 1000000000 / 60 )) minutes ago" )
+    fi
+fi
+
+#---------------------------------
+# SEND ALERTS IF PROBLEMS WERE FOUND
+#---------------------------------
+# if there are alerts
+if [ ${#alerts[@]} -gt 0 ]; then
+
+    # if we haven't sent a message recently
+    last_alert_s=$(date -d $LAST_ALERT +%s)
+    diff=$(( $now_s - $last_alert_s ))
+
+    # time difference is in seconds
+    if [ $diff -ge $(( $ALERT_THRESHOLD * 60 )) ];
+    then
+
+        message="\`\`\`Block Producer Alert (${ALERT_THRESHOLD} minute frequency)\n---------------------------------------"
+
+        for i in "${alerts[@]}"
+        do
+            message="${message}\n- ${i}"
+        done
+
+        message="${message}\n---------------------------------------\`\`\`"
+
+        # send alert
+        curl -H "Content-Type: application/json" -X POST -d '{"username": "SYSTEM", "content": "'"${message}"'"}' ${DISCORD_CHANNEL}
+
+        # update the timestamp
+        sed -i "s/LAST_ALERT=.*/LAST_ALERT=$now/" $CONFIG_FILE_PATH
+
+    fi
+fi
+
+#---------------------------------
+# SEND DAILY SUMMARY
+#---------------------------------
+if [ $(date +%H:%M) == $DAILY_STATUS_AT ]; then
+    summary="\`\`\`Daily Summary\n---------------------------------------"
+    summary="${summary}\nCron job is still running."
+    summary="${summary}\n---------------------------------------\`\`\`"
+
+    # send summary
+    curl -H "Content-Type: application/json" -X POST -d '{"username": "SYSTEM", "content": "'"${summary}"'"}' ${DISCORD_CHANNEL}
+
+    # update the timestamp
+    sed -i "s/LAST_STATUS=.*/LAST_STATUS=$now/" $CONFIG_FILE_PATH
+fi
