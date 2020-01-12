48236bd9eb3b2e1b646b7c7842f50e54d57362e5
Serg Chernata <sergchernata@gmail.com>
2020-01-12 12:23:14 -0500

    added automated failover api endpoints

--- auto-unregprod/auto-unregprod.sh
+++ auto-unregprod/auto-unregprod.sh
@@ -17,17 +17,52 @@ then
 fi
 
 #---------------------------------
+# FUNCTION DECLARATIONS
+#---------------------------------
+get_producer_data () {
+    for api in "${APIS[@]}"
+    do
+        result="$(remcli -u $api get table rem rem producers -L $PRODUCER_NAME -U $PRODUCER_NAME)"
+
+        if [[ $result == *"$PRODUCER_NAME"* ]]; then
+            echo "$result"
+            break
+        fi
+    done
+}
+
+unregister () {
+    for api in "${APIS[@]}"
+    do
+        result="$(remcli -u $api system unregprod $PRODUCER_NAME -p $PRODUCER_NAME@$PERMISSION)"
+
+        if [[ $result == *"executed transaction"* ]]; then
+            echo "$result"
+            break
+        fi
+    done
+}
+
+#---------------------------------
 # COMPARE TIMESTAMPS & UNREGISTER
 #---------------------------------
 now="$(date +%s)"
-last_produced="$(remcli -u https://remchain.remme.io get table rem rem producers -L $PRODUCER_NAME -U $PRODUCER_NAME | jq '.rows[0].last_block_time' -r)"
+producer_data="$(get_producer_data)"
+is_active="$(jq '.rows[0].is_active' <<< ${producer_data})"
+last_produced="$(jq '.rows[0].last_block_time' -r <<< ${producer_data})"
 last_produced_time=$(date -d $last_produced +"%s")
 difference=$(( ($now-$last_produced_time) / 60))
 
-if [[ $difference > $THRESHOLD ]]; then
+if [[ $difference > $THRESHOLD ]] && [ "$is_active" -eq "1" ]; then
+
     remcli wallet unlock < /root/walletpass
-    remcli -u https://remchain.remme.io system unregprod $PRODUCER_NAME -p $PRODUCER_NAME@$PERMISSION
-    echo "${timestamp} - Unregistered due to ${difference} minutes of missed blocks."
+    unreg_result="$(unregister)"
+
+    if [[ $unreg_result == *"executed transaction"* ]]; then
+        echo "${timestamp} - Unregistered due to ${difference} minutes of missed blocks."
+    else
+        echo "${timestamp} - Attempted to unregister due to ${difference} minutes of missed blocks, but all API endpoints failed."
+    fi
 fi
 
 # update the timestamp
